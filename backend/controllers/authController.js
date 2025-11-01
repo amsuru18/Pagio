8aeebcf0ab328f56364fc3acfcfe72913a7667bd
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-01 17:05:39 +0530

    feat(profile): add update profile controller logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -76,8 +76,8 @@ exports.getProfile = async (req, res) => {
       name: user.name,
       email: user.email,
       avatar: user.avatar,
-      isPro: user.isPro
-    })
+      isPro: user.isPro,
+    });
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
@@ -88,6 +88,20 @@ exports.getProfile = async (req, res) => {
 // @access - Private
 exports.updateUserProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.user.id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+
+      const updatedUser = await user.save();
+
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
