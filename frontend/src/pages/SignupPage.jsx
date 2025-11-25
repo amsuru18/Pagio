b8c21bd311a81a8d22875e0bc8efb88862cb6d49
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-25 18:10:23 +0530

    feat(SignupPage): implement necessary imports for SignupPage

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import { useState } from "react";
+import {Link, useNavigate} from "react-router-dom"
+import {Mail, Lock, User, BookOpen} from "lucide-react"
+import toast from "react-hot-toast"
+
+import InputField from "../components/ui/InputField.jsx"
+import Button from "../components/ui/Button.jsx"
+import {useAuth} from "../context/AuthContext.jsx"
+import axiosInstance from "../utils/axiosInstance.js"
+import { API_PATHS } from "../utils/apiPaths.js";
 
 const SignupPage = () => {
     return <div>Signup Page</div>;
