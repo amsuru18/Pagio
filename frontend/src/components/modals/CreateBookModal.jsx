fd0b88da01e6d0accefac00e5bf735028f675ad4
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-27 00:09:30 +0530

    feat(CreateBookModal): implement addChapter, deleteChapter, deleteChapter handeller

--- frontend/src/components/modals/CreateBookModal.jsx
+++ frontend/src/components/modals/CreateBookModal.jsx
@@ -9,16 +9,67 @@ import {
     Lightbulb,
     Palette
 } from 'lucide-react';
-import Modal from "../ui/Modal.jsx"
-import InputField from "../ui/InputField.jsx"
-import SelectedField from "../ui/SelectField.jsx"
-import Button from "../ui/Button.jsx"
+import Modal from '../ui/Modal.jsx';
+import InputField from '../ui/InputField.jsx';
+import SelectedField from '../ui/SelectField.jsx';
+import Button from '../ui/Button.jsx';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import toast from 'react-hot-toast';
 import { useAuth } from '../../context/AuthContext';
 
-const CreateBookModal = () => {
+const CreateBookModal = ({ isOpen, onClose, onBookCreated }) => {
+    const { user } = useAuth();
+
+    const [step, setStep] = useState(1);
+    const [bookTitle, setBookTitle] = useState('');
+    const [numChapters, setNumChapters] = useState(5);
+    const [aiTopic, setAiTopic] = useState('');
+    const [aiStyle, setAiStyle] = useState('Informative');
+    const [chapters, setChapters] = useState([]);
+    const [isGeneralOutline, setIsGeneralOutline] = useState(false);
+    const [isFinalizingBook, setIsFinalizingBook] = useState(false);
+    const chaptersContainerRef = useRef(null);
+
+    const resetModal = () => {
+        setStep(1);
+        setBookTitle('');
+        setNumChapters('');
+        setAiTopic('');
+        setAiStyle('Informative');
+        setChapters([]);
+        setIsGeneralOutline(false);
+        setIsFinalizingBook(false);
+    };
+
+    const handleGenerateOutline = async () => {};
+
+    const handleChapterChange = (index, field, value) => {
+        const updatedChapter = [...chapters];
+        updatedChapters[index][field] = value;
+        setChapters(updatedChapter);
+    };
+
+    const handleDeleteChapter = (index) => {
+        if (chapters.length <= 1) return;
+        setChapters(chapters.filter((_, i) => i !== index));
+    };
+
+    const handleAddChapter = () => {
+        setChapters([
+            ...chapters,
+            {title: `Chapter ${chapters.length + 1}`, description: ""}
+        ])
+    }
+
+    const handleFinalizeBook = async() =>{}
+
+    useEffect(() => {
+        if (step === 2 && chaptersContainerRef.current) {
+            
+        }
+    })
+
     return <div></div>;
 };
 
