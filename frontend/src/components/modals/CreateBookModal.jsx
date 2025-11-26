365373af638e898fae93445e1b24707841d2e4fd
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-26 19:45:35 +0530

    feat(createBookModal): implement imports

--- frontend/src/components/modals/CreateBookModal.jsx
+++ frontend/src/components/modals/CreateBookModal.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useState, useRef, useEffect } from 'react';
+import {
+    Plus,
+    Sparkles,
+    Trash2,
+    ArrowLeft,
+    BookOpen,
+    Hash,
+    Lightbulb,
+    Palette
+} from 'lucide-react';
+import Modal from "../ui/Modal.jsx"
+import InputField from "../ui/InputField.jsx"
+import SelectedField from "../ui/SelectField.jsx"
+import Button from "../ui/Button.jsx"
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import toast from 'react-hot-toast';
+import { useAuth } from '../../context/AuthContext';
 
 const CreateBookModal = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return <div></div>;
+};
 
-export default CreateBookModal
+export default CreateBookModal;
